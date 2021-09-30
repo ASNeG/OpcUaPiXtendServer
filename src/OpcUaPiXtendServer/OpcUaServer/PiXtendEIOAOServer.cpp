eb556f401c745aada06bf10d7543245342dea86f
Samuel Huebl <samuel@huebl-sgh.de>
2021-09-30 06:35:49 -0700

    add pixtend server modules for eIO AO/DO and V2L

--- src/OpcUaPiXtendServer/OpcUaServer/PiXtendEIOAOServer.cpp
+++ src/OpcUaPiXtendServer/OpcUaServer/PiXtendEIOAOServer.cpp
@@ -16,13 +16,51 @@
           Samuel Huebl (Samuel@huebl-sgh.de)
  */
 
+#include "OpcUaStackCore/BuildInTypes/OpcUaIdentifier.h"
+#include "OpcUaStackServer/ServiceSetApplication/CreateObjectInstance.h"
 #include "OpcUaPiXtendServer/OpcUaServer/PiXtendEIOAOServer.h"
+
+using namespace OpcUaStackCore;
+using namespace OpcUaStackServer;
 
 namespace OpcUaPiXtendServer
 {
 
     PiXtendEIOAOServer::PiXtendEIOAOServer(void)
     {
+        ServerVariable::SPtr serverVariable;
+
+        // register analog input variables
+        serverVariable = boost::make_shared<ServerVariable>("AI_AI0_Variable");
+        serverVariables().registerServerVariable(serverVariable);
+        serverVariable = boost::make_shared<ServerVariable>("AI_AI1_Variable");
+        serverVariables().registerServerVariable(serverVariable);
+        serverVariable = boost::make_shared<ServerVariable>("AI_AI2_Variable");
+        serverVariables().registerServerVariable(serverVariable);
+        serverVariable = boost::make_shared<ServerVariable>("AI_AI3_Variable");
+        serverVariables().registerServerVariable(serverVariable);
+        serverVariable = boost::make_shared<ServerVariable>("AI_AI4_Variable");
+        serverVariables().registerServerVariable(serverVariable);
+        serverVariable = boost::make_shared<ServerVariable>("AI_AI5_Variable");
+        serverVariables().registerServerVariable(serverVariable);
+        serverVariable = boost::make_shared<ServerVariable>("AI_AI6_Variable");
+        serverVariables().registerServerVariable(serverVariable);
+        serverVariable = boost::make_shared<ServerVariable>("AI_AI7_Variable");
+        serverVariables().registerServerVariable(serverVariable);
+
+        // register analog output variables
+        serverVariable = boost::make_shared<ServerVariable>("AO_AO0_Variable");
+        serverVariables().registerServerVariable(serverVariable);
+        serverVariable = boost::make_shared<ServerVariable>("AO_AO1_Variable");
+        serverVariables().registerServerVariable(serverVariable);
+        serverVariable = boost::make_shared<ServerVariable>("AO_AO2_Variable");
+        serverVariables().registerServerVariable(serverVariable);
+        serverVariable = boost::make_shared<ServerVariable>("AO_AO3_Variable");
+        serverVariables().registerServerVariable(serverVariable);
+        serverVariable = boost::make_shared<ServerVariable>("AO_AO4_Variable");
+        serverVariables().registerServerVariable(serverVariable);
+        serverVariable = boost::make_shared<ServerVariable>("AO_AO5_Variable");
+        serverVariables().registerServerVariable(serverVariable);
     }
 
     PiXtendEIOAOServer::~PiXtendEIOAOServer(void)
@@ -31,21 +69,32 @@ namespace OpcUaPiXtendServer
 
     bool
 	PiXtendEIOAOServer::startup(
-		const std::string& instanceName,
-		const OpcUaStackCore::OpcUaNodeId& parentNodeId
+	    OpcUaStackServer::ApplicationServiceIf* applicationServiceIf,
+	    const std::string& instanceName,
+	    const std::string& namespaceName,
+	    uint16_t namespaceIndex,
+	    const OpcUaStackCore::OpcUaNodeId& parentNodeId,
+	    uint32_t moduleAddress
 	)
     {
-        uint32_t moduleAddress = 0;
+        applicationServiceIf_ = applicationServiceIf;
+        instanceName_ = instanceName;
+        namespaceName_ = namespaceName;
+        namespaceIndex_ = namespaceIndex;
+        parentNodeId_ = parentNodeId;
 
-    	// get pixtend v2s access interface
+    	// get pixtend eIO AO access interface
     	pixtend_ = PiXtendModulesFactory::createPiXtendEIOAO();
 
     	// startup pixtend interface
     	pixtend_->startup(moduleAddress);
 
-    	// FIXME: TBD
+        // create object instance in information model
+        if (!createObjectInstance()) {
+            return false;
+        }
 
-    	return true;
+        return true;
     }
 
     bool
@@ -57,6 +106,35 @@ namespace OpcUaPiXtendServer
     	// FIXME: TBD
 
     	return true;
+    }
+
+    bool
+    PiXtendEIOAOServer::createObjectInstance(void)
+    {
+        //
+        // create v2s object instance in opc ua information model
+        //
+        objectTypeNamespaceName(namespaceName_);
+        objectTypeNodeId(OpcUaNodeId(1002, namespaceIndex_));
+        Object::SPtr obj = shared_from_this();
+        CreateObjectInstance createObjectInstance(
+            namespaceName_,                                 // namespace name of the object instance
+            OpcUaLocalizedText("", instanceName_),          // display name of the object instance
+            parentNodeId_,                                  // parent node of the object instance
+            OpcUaNodeId(OpcUaId_Organizes),                 // reference type between object and variable instance
+            obj
+        );
+        if (!createObjectInstance.query(applicationServiceIf_)) {
+            Log(Error, "create PiXtendV2S object response error (query)");
+            return false;
+        }
+        if (createObjectInstance.resultCode() != Success) {
+            Log(Error, "create PiXtendV2S object response error (result code)")
+                .parameter("ResultCode", createObjectInstance.resultCode());
+            return false;
+        }
+
+        return true;
     }
 
 }
--- src/OpcUaPiXtendServer/OpcUaServer/PiXtendV2LServer.h
+++ src/OpcUaPiXtendServer/OpcUaServer/PiXtendV2LServer.h
@@ -20,14 +20,18 @@
 #define __OpcUaPiXtendServer_PiXtendV2LServer_h__
 
 #include <boost/shared_ptr.hpp>
-
-#include "OpcUaPiXtendServer/PiXtend/PiXtendModulesFactory.h"
+#include <boost/enable_shared_from_this.hpp>
 #include "OpcUaStackCore/BuildInTypes/OpcUaNodeId.h"
+#include "OpcUaStackServer/Application/ApplicationIf.h"
+#include "OpcUaStackServer/StandardObjectType/ObjectBase.h"
+#include "OpcUaPiXtendServer/PiXtend/PiXtendModulesFactory.h"
 
 namespace OpcUaPiXtendServer
 {
 
     class PiXtendV2LServer
+    : public OpcUaStackServer::ObjectBase
+    , public boost::enable_shared_from_this<PiXtendV2LServer>
     {
       public:
 
@@ -37,13 +41,23 @@ namespace OpcUaPiXtendServer
         ~PiXtendV2LServer(void);
 
         bool startup(
+            OpcUaStackServer::ApplicationServiceIf* applicationServiceIf,
             const std::string& instanceName,
-			const OpcUaStackCore::OpcUaNodeId& parentNodeId
+            const std::string& namespaceName,
+            uint16_t namespaceIndex,
+            const OpcUaStackCore::OpcUaNodeId& parentNodeId
 		);
         bool shutdown(void);
 
       private:
+        OpcUaStackServer::ApplicationServiceIf* applicationServiceIf_ = nullptr;
+        std::string namespaceName_ = "";
+        uint16_t namespaceIndex_ = 0;
         PiXtendV2L::SPtr pixtend_ = nullptr;
+        std::string instanceName_ = "";
+        OpcUaStackCore::OpcUaNodeId parentNodeId_;
+
+        bool createObjectInstance(void);
     };
 
 }
